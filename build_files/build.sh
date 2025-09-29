a2dc486755535fdf155dfe3c1a740891e13266c3
Valerie <155010216+valerie-tar-gz@users.noreply.github.com>
2025-09-29 18:36:16 +0100

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -23,37 +23,6 @@ dnf5 install -y tuigreet flatpak micro
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
 
-#Kernel stuff
-
-AKMODS_FLAVOR="bazzite"
-KERNEL="6.16.4-114.fc42.x86_64"
-skopeo copy --retry-times 3 docker://ghcr.io/ublue-os/akmods:"${AKMODS_FLAVOR}"-"$(rpm -E %fedora)"-"${KERNEL}" dir:/tmp/akmods
-AKMODS_TARGZ=$(jq -r '.layers[].digest' </tmp/akmods/manifest.json | cut -d : -f 2)
-tar -xvzf /tmp/akmods/"$AKMODS_TARGZ" -C /tmp/
-mv /tmp/rpms/* /tmp/akmods/
-
-dnf5 -y install /tmp/kernel-rpms/kernel{,-core,-modules,-modules-core,-modules-extra}-"${KERNEL}".rpm
-
-dnf5 versionlock add kernel{,-core,-modules,-modules-core,-modules-extra,-tools,-tools-lib,-headers,-devel,-devel-matched}
-
-sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/_copr_ublue-os-akmods.repo
-AKMODS=(
-    /tmp/akmods/kmods/*xone*.rpm
-    /tmp/akmods/kmods/*framework-laptop*.rpm
-    /tmp/akmods/kmods/*openrazer*.rpm
-)
-dnf5 -y install "${AKMODS[@]}"
-
-dnf5 -y install \
-        https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-"$(rpm -E %fedora)".noarch.rpm \
-        https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-"$(rpm -E %fedora)".noarch.rpm
-
-dnf5 -y install \
-        v4l2loopback /tmp/akmods/kmods/*v4l2loopback*.rpm
-
-dnf5 -y remove rpmfusion-free-release rpmfusion-nonfree-release
-
-
 #### Example for enabling a System Unit File
 
 systemctl enable podman.socket
