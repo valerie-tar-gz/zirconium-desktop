a53d9c69c60dc89533aa931c802306f13ad3663c
Tulip Blossom <tulilirockz@posteo.com>
2025-10-14 21:23:21 -0300

    fix(flatpak): dont touch flatpak
    Signed-off-by: Tulip Blossom <tulilirockz@posteo.com>

--- build_files/99-cleanup.sh
+++ build_files/99-cleanup.sh
@@ -23,11 +23,6 @@ s|^DEFAULT_HOSTNAME=.*|DEFAULT_HOSTNAME="zirconium"|
 EOF
 
 # GO AWAY fedora flatpaks.
-rm -f /usr/lib/systemd/system/flatpak-add-fedora-repos.service
-mkdir -p /etc/flatpak/remotes.d/
-dnf remove -y fedora-flathub-remote fedora-third-party
-mkdir -p /etc/flatpak/remotes.d/
-curl --retry 3 -Lo /etc/flatpak/remotes.d/flathub.flatpakrepo https://dl.flathub.org/repo/flathub.flatpakrepo
 rm -rf /usr/lib/systemd/system/flatpak-add-fedora-repos.service
 
 KERNEL_VERSION="$(find "/usr/lib/modules" -maxdepth 1 -type d ! -path "/usr/lib/modules" -exec basename '{}' ';' | sort | tail -n 1)"
