757fd4f5c938881740b4cf8ed6d4d807de00da2b
Tulip Blossom <tulilirockz@outlook.com>
2025-10-30 04:01:20 -0300

    feat: use cliphist service for clipboard history on DMS
    Signed-off-by: Tulip Blossom <tulilirockz@outlook.com>

--- build_files/01-theme.sh
+++ build_files/01-theme.sh
@@ -91,6 +91,7 @@ sed -i "s/After=.*/After=graphical-session.target/" /usr/lib/systemd/user/plasma
 add_wants_niri() {
     sed -i "s/\[Unit\]/\[Unit\]\nWants=$1/" "/usr/lib/systemd/user/niri.service"
 }
+add_wants_niri cliphist.service
 add_wants_niri plasma-polkit-agent.service
 add_wants_niri swayidle.service
 add_wants_niri udiskie.service
@@ -115,6 +116,7 @@ systemctl enable --global chezmoi-init.service
 systemctl enable --global app-com.mitchellh.ghostty.service
 systemctl enable --global chezmoi-update.timer
 systemctl enable --global dms.service
+systemctl enable --global cliphist.service
 systemctl enable --global plasma-polkit-agent.service
 systemctl enable --global swayidle.service
 systemctl enable --global udiskie.service
@@ -122,7 +124,7 @@ systemctl enable --global xwayland-satellite.service
 systemctl preset --global app-com.mitchellh.ghostty.service
 systemctl preset --global chezmoi-init
 systemctl preset --global chezmoi-update
-systemctl preset --global dms
+systemctl preset --global cliphist
 systemctl preset --global plasma-polkit-agent
 systemctl preset --global swayidle
 systemctl preset --global udiskie
